67a23e93d48390b43cbaee7f8ce52ab330070695
Mat_Fisher <matvey@rybakov.pro>
2023-03-28 11:48:24 +0300

    Версия 3.0 Добавил ставку пользователя

--- 68_GladiatorFight/Program.cs
+++ 68_GladiatorFight/Program.cs
@@ -10,11 +10,11 @@ int armor2 = rand.Next(30, 60);
 
 Console.WriteLine($"Первый гладиатор:\n\n{health1} Здоровья\n{damage1} Силы удара\n{armor1} Брони\n");
 Console.WriteLine($"Второй гладиатор:\n\n{health2} Здоровья\n{damage2} Силы удара\n{armor2} Брони\n");
-Console.WriteLine("Кто победит?\nВведи номер гладиатора - ");
+Console.Write("Кто победит?\nВведи номер гладиатора - ");
 int userBet = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Нажимайте пробел для удара\n");
 
-int result = 0
+int result = 0;
 while (health1 > 0 && health2 > 0) 
 {
     Console.ReadKey();
@@ -41,6 +41,6 @@ else if (health2 <= 0)
     Console.WriteLine("Второй гладиатор пал");
     result = 1;
 }
-if (userBet = result) Console.WriteLine("Ты был прав!");
+if(userBet == result) Console.WriteLine("Ты был прав!");
 else Console.WriteLine("Ставка не сработала. Попробуй еще.");
 Console.ReadKey();
