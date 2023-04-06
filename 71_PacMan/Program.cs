02e3777194fa87dd868342f5baa7edafa8075061
Mat_Fisher <matvey@rybakov.pro>
2023-04-06 14:51:53 +0300

    Версия 2.0 без решения проблемы "Вас съели"

--- 71_PacMan/Program.cs
+++ 71_PacMan/Program.cs
@@ -1,18 +1,25 @@
 ﻿// Игра Pac-Man
 
 int pacmanX, pacmanY;
+int ghostX, ghostY;
 int pacmanDX = 0, pacmanDY = 1;
+int ghostDX = 0, ghostDY = -1;
+int allDots = 0, colDots = 0;
+bool isAlive = true;
 bool isPlaying = true;
 Console.CursorVisible = false;
+Random random = new Random();
 
 
-char[,] map = ReadMap("Map1", out pacmanX, out pacmanY);
+char[,] map = ReadMap("Map1", out pacmanX, out pacmanY, out ghostX, out ghostY, ref allDots);
 
 
 PrintMap(map);
 
 while (isPlaying)
 {
+    Console.SetCursorPosition(0, 30);
+    Console.WriteLine($"Собрано {colDots}/{allDots}");
     DrawPlayer(pacmanY, pacmanX);
     if (Console.KeyAvailable)
     {
@@ -21,15 +28,48 @@ while (isPlaying)
     }
     if (map[pacmanDX + pacmanX, pacmanDY + pacmanY] != '#')
     {
-        Move(ref pacmanX, ref pacmanY, pacmanDX, pacmanDY);
+        if (map[pacmanX, pacmanY] == '.')
+        {
+            colDots++;
+            map[pacmanX, pacmanY] = ' ';
+        }
+
+        Move(map, '@', ref pacmanX, ref pacmanY, pacmanDX, pacmanDY);
     }
-    System.Threading.Thread.Sleep(100);
+
+
+    if (map[ghostDX + ghostX, ghostDY + ghostY] != '#')
+    {
+        Move(map, '$', ref ghostX, ref ghostY, ghostDX, ghostDY);
+    }
+    else
+    {
+        ChangeDirection(random, ref ghostDX, ref ghostDY);
+    }
+
+    if(ghostX == pacmanX && ghostY == pacmanY)
+    {
+        isAlive = false;
+    }
+
+    System.Threading.Thread.Sleep(200);
+
+    if(colDots == allDots || !isAlive)
+    {
+        isPlaying = false;
+    }
 }
 
+Console.SetCursorPosition(0, 35);
+if (colDots == allDots) Console.WriteLine("You WIN!!!");
+else if (isAlive) Console.WriteLine("You LOSE!!!");
+Console.ReadKey();
 
-char[,] ReadMap(string mapName, out int pacmanX, out int pacmanY)
+
+char[,] ReadMap(string mapName, out int pacmanX, out int pacmanY, out int ghostX, out int ghostY, ref int allDots)
 {
     pacmanX = 0; pacmanY = 0;
+    ghostX = 0; ghostY = 0; 
     string[] newfile = File.ReadAllLines($"Maps/{mapName}.txt");
     char[,] map = new char[newfile.Length, newfile[0].Length];
 
@@ -41,6 +81,17 @@ char[,] ReadMap(string mapName, out int pacmanX, out int pacmanY)
             if (map[i,j] == '@')
             {
                 pacmanX = i; pacmanY = j;
+                map[i, j] = '.';
+            }
+            else if (map[i, j] == '$')
+            {
+                ghostX = i; ghostY = j;
+                map[i, j] = '.';
+            }
+            else if (map[i, j] == ' ')
+            {
+                map[i, j] = '.';
+                allDots++;
             }
         }
     }
@@ -65,16 +116,16 @@ void DrawPlayer(int pacmanY, int pacmanX)
     Console.Write('@');
 }
 
-void Move(ref int x, ref int y, int dx, int dy)
+void Move(char [,] map, char simbol, ref int x, ref int y, int dx, int dy)
 {
     Console.SetCursorPosition(y, x);
-    Console.Write(" ");
+    Console.Write(map[x,y]);
 
     x += dx;
     y += dy;
 
     Console.SetCursorPosition(y, x);
-    Console.Write("@");
+    Console.Write(simbol);
 }
 
 void PacmanMove(ConsoleKeyInfo key, ref int dx, ref int dy)
@@ -91,6 +142,27 @@ void PacmanMove(ConsoleKeyInfo key, ref int dx, ref int dy)
             dx = 0; dy = -1;
             break;
         case ConsoleKey.RightArrow:
+            dx = 0; dy = 1;
+            break;
+    }
+}
+
+void ChangeDirection(Random random, ref int dx, ref int dy)
+{
+    int ghostDir = random.Next(1, 5);
+
+    switch (ghostDir)
+    {
+        case 1:
+            dx = -1; dy = 0;
+            break;
+        case 2:
+            dx = 1; dy = 0;
+            break;
+        case 3:
+            dx = 0; dy = -1;
+            break;
+        case 4:
             dx = 0; dy = 1;
             break;
     }
